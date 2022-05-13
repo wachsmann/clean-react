313f935a97bf322270194412cca3933d6e5e359f
wachsmann <vhwachsmann@gmail.com>
2022-05-12 22:55:23 -0300

    fix: github copy conflicts

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,19 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { RemoteAuthentication } from './remote-authentication'
+
+export class HttpPostClientSpy implements HttpPostClient {
+    url?: string
+    async post (url: string): Promise<void> {
+      this.url = url
+      return Promise.resolve()
+    }
+}
+describe('RemoteAuthentication', () => {
+  test('Should call HttpPostClient with correct URL ', async () => {
+    const url = 'any_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
