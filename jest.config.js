3002f2df8dcd6513cacdb7bbcd6eddeec54f5e7e
wachsmann <vhwachsmann@gmail.com>
2022-05-19 23:02:26 -0300

    chore: confic sass
    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
     '.+\\.(ts|tsx))$': 'ts-jest'
   },
   moduleNameMapper : {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
