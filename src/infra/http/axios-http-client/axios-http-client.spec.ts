54d8ca7615b34075ec849986eabb242a8e99f5f4
wachsmann <vhwachsmann@gmail.com>
2022-05-19 21:46:34 -0300

    refactor: move axios mock to a separate file

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,30 +1,30 @@
 import { AxiosHttpClient } from './axios-http-client'
+import { mockAxios } from '@/infra/test'
 import axios from 'axios'
-import faker from '@faker-js/faker'
-import { HttpPostParams } from '@/data/protocols/http'
+import { mockPostRequest } from '@/data/test/mock-http-post'
 
 jest.mock('axios')
-const mockedAxios = axios as jest.Mocked<typeof axios>
-const mockedAxiosResult = { data: undefined, status: faker.datatype.number() }
-mockedAxios.post.mockResolvedValue(mockedAxiosResult)
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
+
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
 }
-const mockPostRequest = (): HttpPostParams<any> => ({ url: faker.internet.url(), body: faker.helpers.objectValue({ mockPostKey: 'mockPostValue' }) })
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
+  return { sut, mockedAxios }
+}
 
 describe('AxiosHttpClient', () => {
   test('Should call Axios with correct values', async () => {
-    const sut = makeSut()
+    const { sut, mockedAxios } = makeSut()
     const request = mockPostRequest()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
-  test('Should return the correct status code and body', async () => {
-    const sut = makeSut()
-    const httpResponse = await sut.post(mockPostRequest())
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosResult.status,
-      body: mockedAxiosResult.data
-    })
+  test('Should return the correct status code and body', () => {
+    const { sut, mockedAxios } = makeSut()
+    const promise = sut.post(mockPostRequest())
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
   })
 })
---
+++ src/infra/test/mock-axios.ts
@@ -0,0 +1,8 @@
+import faker from '@faker-js/faker'
+import axios from 'axios'
+
+export const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+  mockedAxios.post.mockResolvedValue({ data: undefined, status: faker.datatype.number() })
+  return mockedAxios
+}
