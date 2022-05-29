62c3f0c13449927723baf523e4d9d3b37fc32b51
wachsmann <vhwachsmann@gmail.com>
2022-05-29 12:34:09 -0300

    Revert "Revert "feat: ensure Login start with inputs with required state""
    This reverts commit a0640da13b257f0cd9b2b261116fa278ec0517f6.

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react'
 import { Input, Footer, FormStatus, LoginHeader } from '@/presentation/components'
 import Styles from './login-styles.scss'
 import Context from '@/presentation/components/context/form/form-context'
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
 
+const Login: React.FC = () => {
+  const [state] = useState({
+    isLoading: false
+  })
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={state}>
+      <Context.Provider value={{ state, errorState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder='Digite seu e-mail' />
