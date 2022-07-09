1ebdbbd4513fe9d75a82251b46430807fc54eba7
wachsmann <vhwachsmann@gmail.com>
2022-07-09 15:22:49 -0300

    test: ensure required field validation returns false if field is not empty

--- src/validation/required-field/required-field-validation.spec.tsx
+++ src/validation/required-field/required-field-validation.spec.tsx
@@ -1,3 +1,4 @@
+import faker from '@faker-js/faker'
 import { RequiredFieldError } from '../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 describe('RequiredFieldValidation', () => {
@@ -5,5 +6,10 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError(''))
+  })
+  test('should return false if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
