6834d768a66ff00963a5043fbc958398b73598a1
wachsmann <vhwachsmann@gmail.com>
2022-07-10 16:55:51 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,4 +7,9 @@ describe('MinLength', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
   })
+  test('should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
 })
