9d1e4abf6f6a80af7f090a460d52cb8892408a24
wachsmann <vhwachsmann@gmail.com>
2022-07-10 16:06:29 -0300

    feat: ensure email validation returns error if email is invalid

--- src/validation/validators/required-field/required-field-validation.ts
+++ src/validation/validators/required-field/required-field-validation.ts
@@ -1,6 +1,5 @@
 import { RequiredFieldError } from '@/validation/errors'
 import { FieldValidation } from '../../protocols/field-validation'
-
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) { }
   validate (value: string): Error {
