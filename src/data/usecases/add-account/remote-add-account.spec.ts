953b960831154fa66875d1fe3615130b778c3f52
wachsmann <vhwachsmann@gmail.com>
2022-10-20 22:05:37 -0300

    test: ensure RemoteAddAccount call post with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,27 @@
+import { AccountModel } from '@/domain/models'
+import { mockAddAccountParams } from '@/domain/test'
+import { AddAccountParams } from '@/domain/usecases/add-account'
+import { HttpPostClientSpy } from '@/infra/http/axios-http-client/axios-http-client'
+import faker from '@faker-js/faker'
+import { RemoteAddAccount } from './remote-add-account'
+
+type SutTypes = {
+  sut: RemoteAddAccount
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+}
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<AddAccountParams, AccountModel>()
+  const sut = new RemoteAddAccount(url,httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+describe('RemoteAddAccount', () => {
+  test('Should call HttpPostClient with correct URL ', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.add(mockAddAccountParams())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,12 @@
+import faker from '@faker-js/faker'
+import { AddAccountParams } from '../usecases/add-account'
+
+export const mockAddAccountParams = (): AddAccountParams => {
+  const password = faker.internet.password()
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
