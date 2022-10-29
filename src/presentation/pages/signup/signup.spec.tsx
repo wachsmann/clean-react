5a7ef6a31db7a2be156e759f3e5b531ec65f119f
wachsmann <vhwachsmann@gmail.com>
2022-10-29 13:44:30 -0300

    test: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -39,4 +39,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name', validationError)
   })
+  test('should show email error if Validation fails ', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email', validationError)
+  })
 })
