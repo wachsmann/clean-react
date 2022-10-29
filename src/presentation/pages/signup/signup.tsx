6fbf1491269103a968f25d484aa0af59c21c185e
wachsmann <vhwachsmann@gmail.com>
2022-10-29 14:23:29 -0300

    feat: ensure SignUp shows spinner on submit

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -31,11 +31,26 @@ const SignUp: React.FC<Props> = ({ validation }) => {
       }
     })
   }, [state.name, state.email, state.password, state.passwordConfirmation])
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault()
+    try {
+      if (state.isLoading || state.emailError || state.passwordError || state.nameError || state.passwordConfirmationError) {
+        return
+      }
+      setState({ ...state, isLoading: true })
+    } catch (error) {
+      setState({
+        ...state,
+        isLoading: false,
+        mainError: error.message
+      })
+    }
+  }
   return (
     <div className={Styles.signup}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form} >
+        <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
           <h2>Criar conta</h2>
           <Input type="text" name="name" placeholder='Digite seu nome' />
           <Input type="email" name="email" placeholder='Digite seu e-mail' />
