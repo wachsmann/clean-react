7a8a3959436cdc7af50eaa6478a2b7cee1b6e4a7
wachsmann <vhwachsmann@gmail.com>
2022-11-02 10:12:19 -0300

    test: ensure Login Link goes to right place

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -172,4 +172,12 @@ describe('SignUp Component', () => {
       Helper.testElementText(sut, 'main-error', error.message)
     })
   })
+
+  test('should go to login page', () => {
+    const { sut } = makeSut()
+    const loginLink = sut.getByTestId('login-link')
+    fireEvent.click(loginLink)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/login')
+  })
 })
